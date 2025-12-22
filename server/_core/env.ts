5ed501b024fc1a60b06c1bad142dfc25b76c91c0
xmrpsychoox <bechtelleon007@gmail.com>
2025-12-22 04:39:55 +0100

    Update2

--- server/_core/env.ts
+++ server/_core/env.ts
@@ -5,6 +5,6 @@ export const ENV = {
   oAuthServerUrl: "https://teamliste-final.onrender.com",
   ownerOpenId: process.env.OWNER_OPEN_ID ?? "",
   isProduction: process.env.NODE_ENV === "production",
-  forgeApiUrl: process.env.BUILT_IN_FORGE_API_URL ?? "",
+  forgeApiUrl: "https://teamliste-final.onrender.com",
   forgeApiKey: process.env.BUILT_IN_FORGE_API_KEY ?? "",
 };
